897649778ea8871529af7c1991d60868f1994c72
Markus Faßbender <markus.fassbender@wetteronline.de>
2019-06-15 10:45:25 +0200

    setup empty single view application

---
+++ MusicAlbums/MusicAlbums/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicAlbums
+//
+//  Created by Markus Faßbender on 15.06.19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
