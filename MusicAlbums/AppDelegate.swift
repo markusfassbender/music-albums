c1e7a81f8c9fc24a37347ce45cf527cde58f75d4
Markus Faßbender <markus.fassbender@wetteronline.de>
2019-06-15 10:58:20 +0200

    remove auto generated main storyboard

--- MusicAlbums/AppDelegate.swift
+++ MusicAlbums/AppDelegate.swift
@@ -11,10 +11,20 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        self.window = window
+        
+        window.makeKeyAndVisible()
+        
+        return true
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let viewController = ViewController(nibName: nil, bundle: nil)
+        window?.rootViewController = viewController
+        
         return true
     }
 
