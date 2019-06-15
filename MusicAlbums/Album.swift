bbaaa55ea650ab2755e685fc877b18c90f405f59
Markus Faßbender <markus.fassbender@wetteronline.de>
2019-06-15 12:17:13 +0200

    add album model

---
+++ MusicAlbums/Album.swift
@@ -0,0 +1,15 @@
+//
+//  Album.swift
+//  MusicAlbums
+//
+//  Created by Markus Faßbender on 15.06.19.
+//
+
+import Foundation
+
+struct Album {
+    let title: String
+    let artist: String
+    let tracks: [String]
+    let imageURL: URL
+}
