f9f57557621c8893a3376901277bf76bf1bd63ee
Markus Faßbender <markus.fassbender@wetteronline.de>
2019-06-19 21:24:00 +0200

    add network service build target

---
+++ NetworkService/NetworkService.h
@@ -0,0 +1,18 @@
+//
+//  NetworkService.h
+//  NetworkService
+//
+//  Created by Markus Faßbender on 19.06.19.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetworkService.
+FOUNDATION_EXPORT double NetworkServiceVersionNumber;
+
+//! Project version string for NetworkService.
+FOUNDATION_EXPORT const unsigned char NetworkServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkService/PublicHeader.h>
+
+
