ca7ddd3505450b055a042298b70be3aecdf348b3
Markus Faßbender <markus.fassbender@wetteronline.de>
2019-06-20 23:06:01 +0200

    solve url parsing issue with custom decoding

--- MusicAlbums/LastFM+Image.swift
+++ MusicAlbums/LastFM+Image.swift
@@ -9,22 +9,23 @@ import Foundation
 
 public extension LastFM {
     struct Image: Decodable {
-        public let urlString: String
+        public let url: URL?
         public let size: Size
         
         enum CodingKeys: String, CodingKey {
-            case urlString = "#text"
+            case url = "#text"
             case size
+        }
+        
+        public init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            // try? is a workaround to avoid parsing issues. The API sometimes return invalid URLs like {"#text": ""}. 
+            url = try? container.decode(URL.self, forKey: .url)
+            size = try container.decode(Size.self, forKey: .size)
         }
         
         public enum Size: String, Decodable {
             case small, medium, large, extralarge, mega
         }
-    }
-}
-
-public extension LastFM.Image {
-    var url: URL? {
-        return URL(string: urlString)
     }
 }
