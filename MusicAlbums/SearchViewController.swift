f9395970ae7a87aaa461329176300af8cb61f4b3
Markus Faßbender <markus.fassbender@posteo.de>
2019-12-21 11:02:24 +0100

    refactor search from table view controller to view controller

--- MusicAlbums/SearchViewController.swift
+++ MusicAlbums/SearchViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import Models
 import NetworkService
 
-class SearchViewController: UITableViewController {
+class SearchViewController: UIViewController {
     private var searchCancelToken: CancelToken?
     
     deinit {
@@ -25,11 +25,8 @@ class SearchViewController: UITableViewController {
         view.backgroundColor = Stylesheet.Color.viewBackground
         title = NSLocalizedString("title_search", comment: "")
         
-        tableView.separatorInset = .zero
-        
         let resultsViewcontroller = SearchResultsViewController()
         resultsViewcontroller.delegate = self
-        resultsViewcontroller.tableView.separatorInset = tableView.separatorInset
         
         let searchController = UISearchController(searchResultsController: resultsViewcontroller)
         searchController.searchResultsUpdater = self
