455ef8d8014b655540548baddb1812be5e287674
Markus Faßbender <markus.fassbender@posteo.de>
2020-02-05 11:42:57 +0100

    [ADD] tests artist resource results

--- NetworkServiceTests/ArtistResourceTests.swift
+++ NetworkServiceTests/ArtistResourceTests.swift
@@ -12,7 +12,7 @@ import Models
 
 class ArtistResourceTests: XCTestCase {
     
-    func testArtistResourceAllThrowsException() {
+    func testArtistResourceAllFromEmptyData() {
         let resource = Artist.all(for: "John Lennon")
         XCTAssertThrowsError(try resource.parse(Data()))
     }
@@ -25,5 +25,35 @@ class ArtistResourceTests: XCTestCase {
         let artists = try resource.parse(data)
         
         XCTAssertEqual(artists.count, 2)
+    }
+    
+    func testArtistResourceName() throws {
+        let url = Bundle(for: type(of: self)).url(forResource: "mock_artist", withExtension: "json")!
+        let data = try Data(contentsOf: url)
+        let resource = Artist.all(for: "John Lennon")
+        
+        let artists = try resource.parse(data)
+        
+        XCTAssertEqual(artists.first?.name, "John Lennon")
+    }
+    
+    func testArtistResourceImage() throws {
+        let url = Bundle(for: type(of: self)).url(forResource: "mock_artist", withExtension: "json")!
+        let data = try Data(contentsOf: url)
+        let resource = Artist.all(for: "John Lennon")
+        
+        let artists = try resource.parse(data)
+        
+        XCTAssertEqual(artists.first?.image, nil)
+    }
+    
+    func testArtistResourceImageURL() throws {
+        let url = Bundle(for: type(of: self)).url(forResource: "mock_artist", withExtension: "json")!
+        let data = try Data(contentsOf: url)
+        let resource = Artist.all(for: "John Lennon")
+        
+        let artists = try resource.parse(data)
+        
+        XCTAssertEqual(artists.first?.imageURL?.absoluteString, "https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png")
     }
 }
